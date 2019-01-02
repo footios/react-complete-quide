36fe3d38221f1b4cca01f3b8653e22ebef158682
footios <footios76@gmail.com>
2019-01-02 14:57:15 +0200

    67 Enabling & Using CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import classes from "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { id: "anything", name: "Max", age: 28 },
+      { id: "sdv", name: "Manu", age: 29 },
+      { id: "sssdv", name: "Anna", age: 29 }
+    ],
+    showPersons: false
+  };
+
+  inputHandler = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  nameChangeHandler = (id, event) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons });
+  };
+
   render() {
+    let style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                changed={this.nameChangeHandler.bind(this, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+
+      style.backgroundColor = "blue";
+    }
+
+    let assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red);
+    }
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.App}>
+        <h1>
+          Hi
+          <br /> I'm a Happy <br />
+        </h1>
+        <p className={assignedClasses.join(" ")}>React app</p>
+
+        <button onClick={this.togglePersonHandler} style={style}>
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     );
   }
